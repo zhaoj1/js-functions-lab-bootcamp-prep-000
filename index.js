9afdae2aa596f675008d4a58c155764fba959978
rational-checkpoint-3002 <zhaoj1@ymail.com>
2019-06-01 21:27:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 // write your code below!
 function happyHolidays() {console.log("Happy holidays!")}
 function happyHolidaysTo(firstName) {console.log(`Happy holidays, ${name}!`)}
+function happyHolidayTo(holiday,firstName) {console.log(`Happy ${holiday}, ${name}!`)}
+function holidayCountdown(days,holiday) {console.log(`It's ${days} days until ${holiday}!`)}
